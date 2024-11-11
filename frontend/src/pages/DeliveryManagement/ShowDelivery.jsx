6e8c182f109ba66ccbdc504565ee901eb271b5d1
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-11-11 20:30:50 +0530

    add css for ShowDelivery.jsx

--- frontend/src/pages/DeliveryManagement/ShowDelivery.jsx
+++ frontend/src/pages/DeliveryManagement/ShowDelivery.jsx
@@ -86,6 +86,85 @@ const ShowDelivery = () => {
   );
 };
 
-
+const styles = {
+  pageContainer: {
+    backgroundColor: '#fff',
+    minHeight: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    padding: '0 20px',
+  },
+  container: {
+    padding: '30px',
+    fontFamily: 'Arial, sans-serif',
+    minWidth: '650px',
+    backgroundColor: '#fff',
+    borderRadius: '10px',
+  },
+  title: {
+    fontSize: '30px',
+    margin: '30px 0',
+    textAlign: 'center',
+    color: 'rgb(53 18 18)',
+  },
+  receipt: {
+    border: '1px solid #ccc',
+    borderRadius: '10px',
+    padding: '20px',
+    backgroundColor: '#fff',
+    boxShadow: '0 3px 6px rgba(0, 0, 0, 0.9)',
+    textcolor: '',
+  },
+  header: {
+    textAlign: 'center',
+    marginBottom: '30px',
+    borderBottom: '2px solid #ccc',
+    paddingBottom: '10px',
+  },
+  details: {
+    paddingTop: '20px',
+  },
+  row: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: '12px 0',
+    borderBottom: '1px solid #eee',
+  },
+  label: {
+    fontWeight: 'bold',
+    color: '#444',
+    flex: '1',
+    fontSize: '17px',
+  },
+  value: {
+    color: '#666',
+    flex: '2',
+    textAlign: 'right',
+    fontSize: '18px',
+  },
+  buttonContainer: {
+    marginTop: '50px',
+    display: 'flex',
+    justifyContent: 'space-around',
+  },
+  button: {
+    padding: '10px',
+    fontWeight: 'bold',
+    fontSize: '16px',
+    borderRadius: '40px',
+    cursor: 'pointer',
+    width: '187px', // Fixed width for consistency
+  },
+  deleteButton: {
+    backgroundColor: '#330D0F',
+    color: '#F1EEDA',
+    border: 'none',
+  },
+  editButton: {
+    backgroundColor: '#fff',
+    color: '#330D0F',
+    border: '5px solid #330D0F',
+  },
+};
 
 export default ShowDelivery;
