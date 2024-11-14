22d3b6465170ffaa3d5722ce7cb02e97b1afb888
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-11-14 16:51:07 +0530

    Create SculpturesPage.jsx

---
+++ frontend/src/pages/ProductManagement/SculpturesPage.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ProductSingleCard from "../../components/home/ProductSingleCard";
+import Spinner from '../../components/spinner';
+import { FaSearch } from 'react-icons/fa';
+import Header from '../../components/headerfooter/Header';
+import Footer from '../../components/headerfooter/Footer';
+
+const SculpturesPage = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("http://localhost:5555/products?category=sculpture")
+      .then((response) => {
+        setProducts(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const filteredProducts = products.filter(product =>
+    product.productName.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return (
+    <div>
+      <Header />
+    <div style={styles.pageContainer}>
+      {loading ? <Spinner /> : (
+        <>
+          <div style={styles.searchContainer}>
+            <FaSearch style={styles.searchIcon} />
+            <input
+              type="text"
+              placeholder="Search by product name"
+              value={searchQuery}
+              onChange={handleSearch}
+              style={styles.searchInput}
+            />
+          </div>
+          <div style={styles.productGrid}>
+            {filteredProducts.length > 0 ? (
+              filteredProducts.map((item) => (
+                <ProductSingleCard key={item._id} product={item} />
+              ))
+            ) : (
+              <p>No products available.</p>
+            )}
+          </div>
+        </>
+      )}
+    </div>
+    <Footer />
+    </div>
+  );
+};
+
+
+export default SculpturesPage;
