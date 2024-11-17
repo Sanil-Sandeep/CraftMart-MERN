2b5680eb7835ec981e983e40187aea3e4c82ec33
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-11-17 20:15:21 +0530

    add css for Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -76,5 +76,78 @@ const Login = () => {
     );
 };
 
+const styles = {
+    page: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        height: '100vh',
+    },
+    container: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexGrow: 1,
+    },
+    header: {
+        backgroundColor: '#330D0F',
+        padding: '30px',
+        textAlign: 'center',
+    },
+    headerText: {
+        color: '#fff',
+    },
+    loginContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '15px',
+        margin: '20px 0',
+        border: '1px solid #ccc',
+        borderRadius: '8px',
+        boxShadow: '0 10px 20px rgba(0, 0, 0, 0.2)',
+        backgroundColor: '#ffffff',
+        width: '400px',
+        height: '400px', // Set a fixed height
+        overflow: 'auto',
+    },
+    form: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '10px',
+    },
+    loginHeader: {
+        marginBottom: '50px', // Adjust this value as needed
+        marginTop: '0', // Optional: remove any top margin
+        fontWeight: 'bold', 
+        fontSize: '25px'
+    },
+    input: {
+        padding: '10px',
+        fontSize: '18px',
+        borderRadius: '5px',
+        border: '1px solid #ccc',
+        width: '100%',
+    },
+    button: {
+        marginTop: '20px',
+        padding: '10px',
+        fontSize: '18px',
+        borderRadius: '5px',
+        border: 'none',
+        backgroundColor: '#330D0F',
+        color: '#fff',
+        cursor: 'pointer',
+    },
+    footer: {
+        backgroundColor: '#330D0F',
+        padding: '30px',
+        textAlign: 'center',
+    },
+    footerText: {
+        color: '#fff',
+    },
+};
 
 export default Login;
